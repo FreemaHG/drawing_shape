f8a11743154327b546132da46f5f5a2d2c35bda5
Стафеев Дмитрий <stafeeff.dmitrij@yandex.ru>
2024-02-14 14:26:20 +0300

    Part 6. Add transparency for shapes

--- shapes/base_shape.py
+++ shapes/base_shape.py
@@ -16,4 +16,23 @@ class ShapeBase:
         """
         self.line_width = kwargs.get("line_width", 1)
         self.outline = kwargs.get("outline", (0, 0, 0))
-        self.color = kwargs.get("Color", None)
+        self.transparency = kwargs.get("transparency", None)
+        self._color = kwargs.get("Color", None)
+
+    @property
+    def color(self) -> str | None:
+        """
+        Изменяем цвет в зависимости от прозрачности фигуры
+        """
+
+        if self.transparency == "off" and self._color is None:
+            self._color = "white"
+
+        elif self.transparency == "on":
+            self._color = None
+
+        return self._color
+
+    @color.setter
+    def color(self, new_color: str):
+        self._color = new_color
